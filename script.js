689da8d91a2d005f905a717a3b2f57ec42477fc9
R4vio <amin3bourass@gmail.com>
2020-11-22 23:29:08 +0100

    Added line reading and separating

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 fs = require('fs')
+function separate(sentence){
+    const index = sentence.indexOf(',')
+    return [sentence.slice(0,index) , sentence.slice(index+1)]
+}
 fs.readFile('messages.txt', 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
-    console.log(data)
+    const messages = data.split('\n')
+    let line = messages[Math.floor(Math.random()*messages.length)]
+    line = separate(line)
+    console.log(line)
 });
